3efb09762496a565ed50a194bf89edbc66779ec2
kushal201 <kushalg.2001@gmail.com>
2024-01-01 18:54:15 +0530

    pushing changes on 01/01

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -28,14 +28,13 @@ const RestaurantMenu = () => {
 
     const {name, cuisines, cloudinaryImageId, costForTwoMessage, avgRatingString} = resInfo?.cards[0]?.card?.card?.info;
 
-    const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card ;
-
+    const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card || resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
     console.log(itemCards);
 
     return (
         <div className="menu">
             <h1>{name}</h1>
-            <h5>{cuisines.join(", ")} - {costForTwoMessage}</h5>
+            <h3>{cuisines.join(", ")} - {costForTwoMessage}</h3>
             <h4>{avgRatingString}</h4>
             <ul>
                 {itemCards.map((item) => (<li key = {itemCards[0]?.card?.info?.id}>{item.card?.info?.name} - {item?.card?.info?.price/100} Rs.</li>))}
