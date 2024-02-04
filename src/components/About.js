d2bcf4ba1c6a1fb0789db08877cf9375aba3e510
kushal201 <kushalg.2001@gmail.com>
2024-02-04 17:15:54 +0530

    pushing changes on 04/02

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import User from "./User";
 import UserClass from "./UserClass"
-const About = () => {
-    return(
-        <div>
-            <h1>About</h1>
-            <h1>This is Namaste React</h1>
-            <User/>
-            <UserClass name = {"Kushal (Class)"} location = {"Hyderabad (Class)"}/>
-        </div>
-    );
+import { Component } from "react";
+class About extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        console.log("Parent Component Did Mount")
+    }
+
+    render() {
+        console.log("Parent Render")
+        return(
+            <div>
+                <h1>About</h1>
+                <h1>This is Namaste React</h1>
+                <User/>
+                <UserClass name = {"Kushal (Class)"} location = {"Hyderabad (Class)"}/>
+            </div>
+        );
+    }
+    
 };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -10,15 +10,29 @@ class UserClass extends React.Component {
         }
 
         console.log(props)
+        console.log("Child Constructor");
+    }
+
+    componentDidMount() {
+        console.log("Child component did mount");
     }
 
     render() {
+        console.log("Child Render");
         const {name, location} = this.props;
         const {count, count2} = this.state;
+
         return(
             <div className="user-card">
                 <h1>From Class Component</h1>
                 <h1>Count: {count}</h1>
+                <button onClick = { () => {
+                    this.setState({
+                        count: this.state.count + 1,
+                        count2: this.state.count2 + 2
+                    })
+                }
+                }>change count</button>
                 <h1></h1>
                 <h1>Count2: {count2}</h1>
                 <h2>Name: {name}</h2>
