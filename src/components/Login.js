c227ecf90e912a0ea68419d763258911263dec8d
kushal201 <kushalg.2001@gmail.com>
2024-10-06 20:05:36 +0530

    pushing changes on 06/10

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { checkValidData, checkName } from "../utils/validate";
+import { checkValidData } from "../utils/validate";
 import { useGlobal } from "../utils/UserContext";
 import {
   createUserWithEmailAndPassword,
@@ -11,16 +11,30 @@ import {
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { addUser } from "../utils/userSlice";
+import { LOGIN_IMAGES } from "../utils/constant";
 
 const Login = () => {
   const [signIn, setSignIn] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const { user, setUser } = useGlobal();
   const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === LOGIN_IMAGES.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? LOGIN_IMAGES.length - 1 : prevIndex - 1
+    );
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -30,9 +44,14 @@ const Login = () => {
     return () => unsubscribe();
   }, [setUser]);
 
+  useEffect(() => {
+    const interval = setInterval(nextImage, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   const handleButtonClick = async (e) => {
     e.preventDefault();
-    setErrorMessage(null)
+    setErrorMessage(null);
     // validating email and password
     const message = checkValidData(email.current.value, password.current.value);
     setErrorMessage(message);
@@ -47,19 +66,17 @@ const Login = () => {
           password.current.value
         );
         const newUser = userCredential.user;
-        const {uid, displayName, email} = newUser;
+        const { uid, displayName, email: userEmail } = newUser;
 
         await updateProfile(newUser, {
           displayName: name.current.value,
         });
 
-        // Use newUser instead of user
         dispatch(
-          addUser(
-            {
+          addUser({
             uid: uid,
             displayName: displayName,
-            email: email,
+            email: userEmail,
           })
         );
 
@@ -81,40 +98,65 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-yellow-300 h-[700px]">
-      <form onSubmit={(e) => e.preventDefault()} className="">
-        {!signIn && (
-          <input
-            ref={name}
-            type="text"
-            placeholder="Name"
-            className="p-3 m-3"
-          ></input>
-        )}
-        <input
-          ref={email}
-          type="text"
-          placeholder="Email"
-          className="p-3 m-3"
-        ></input>
-        <input
-          ref={password}
-          type="password"
-          placeholder="Password"
-          className="p-3 m-3"
-        ></input>
-        <p className="text-red-500">{errorMessage}</p>
+    <>
+      <div className="w-screen h-screen overflow-hidden">
+        <img
+          src={LOGIN_IMAGES[currentIndex]}
+          alt={`carousel image ${currentIndex}`}
+          className="object-cover w-full h-full transition-transform duration-500"
+        />
         <button
-          onClick={handleButtonClick}
-          className="ml-3 p-3 bg-gray-100 border border-black rounded-lg"
+          className="absolute left-5 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-full"
+          onClick={prevImage}
         >
-          {signIn ? "Sign In" : "Sign Up"}
+          Prev
         </button>
-        <p className="cursor-pointer" onClick={() => setSignIn(!signIn)}>
-          {signIn ? "Sign Up Now!" : "Sign In"}
-        </p>
-      </form>
-    </div>
+        <button
+          className="absolute right-5 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-full"
+          onClick={nextImage}
+        >
+          Next
+        </button>
+
+          <div className="absolute mright-[40px] z-100 w-[300px] h-[400px] border border-black flex">
+            <form onSubmit={(e) => e.preventDefault()}>
+              {!signIn && (
+                <input
+                  ref={name}
+                  className="p-3 m-3 border border-black"
+                  type="text"
+                  placeholder="Name"
+                />
+              )}
+              <input
+                ref={email}
+                className="p-3 m-3 border border-black"
+                type="text"
+                placeholder="Email"
+              />
+              <input
+                ref={password}
+                className="p-3 m-3 border border-black"
+                type="password"
+                placeholder="Password"
+              />
+              <button
+                onClick={handleButtonClick}
+                className="p-3 m-3 bg-orange-400 text-white rounded-lg"
+              >
+                {signIn ? "Sign In" : "Sign Up"}
+              </button>
+              <p className="mt-3 text-red-600">{errorMessage}</p>
+              <p
+                onClick={() => setSignIn(!signIn)}
+                className="m-4 hover:underline cursor-pointer"
+              >
+                {signIn ? "No Account? Sign Up Today!" : "Sign in"}
+              </p>
+            </form>
+          </div>
+        </div>
+    </>
   );
 };
 
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -8,4 +8,11 @@ export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_M
 
 export const MENU_EXTEND_URL = "&catalog_qa=undefined&submitAction=ENTER";
 
-// export const LOGIN_BG_URL = 
+export const LOGIN_IMAGES = [
+    "https://images.moneycontrol.com/static-mcnews/2023/10/pexels-anil-sharma-10580198.jpg?impolicy=website&width=1600&height=900",
+    "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/topic_centers/2020-7/food-still-life-salmon-keto-healthy-eating-1296x728-header.jpg?w=1155&h=1528",
+    "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?cs=srgb&dl=pexels-chanwalrus-958545.jpg&fm=jpg",
+    "https://png.pngtree.com/background/20230528/original/pngtree-an-arrangement-of-various-indian-food-picture-image_2778221.jpg",
+    "https://webneel.com/wnet/file/images/3-17/3-dosa-idlis-india-food-photography-by-nitin-rai.jpg",
+    "https://smweddingplanners.com/wp-content/uploads/2018/01/best-south-indian-food-wedding-lunch.jpg"
+]; 
